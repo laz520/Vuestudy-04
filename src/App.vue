5b40ae1363d1172732253eaba9a69c85b54e99ad
格雷福斯 <2104819695@qq.com>
2022-11-20 16:49:23 +0800

    tarbar

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+<TabBar>
+ <TabBarItem></TabBarItem>
+  <TabBarItem></TabBarItem>
+  <TabBarItem></TabBarItem>
+  <TabBarItem></TabBarItem>
+
+
+</TabBar>
+  </div>
+</template>
+<script>
+import TabBar from "@/components/tabbar/TabBar"; //导入组件
+import TabBarItem from "@/components/tabbar/TabBarItem";
+export default {
+
+  components:{
+    TabBar  , //注册组件
+    TabBarItem,
+  }
+}
+</script>
+<style>
+@import "assets/css/base.css";
+</style>
